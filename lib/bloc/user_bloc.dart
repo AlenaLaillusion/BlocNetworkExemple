83540b62315ca1d091d18003d7731e52fad6b8ad
helen.dikovitsky@gmail.com <helen.dikovitsky@gmail.com>
2021-08-03 10:10:55 +0300

    bloc_network_excemple with lib flutter_bloc_6.0

--- lib/bloc/user_bloc.dart
+++ lib/bloc/user_bloc.dart
@@ -5,26 +5,47 @@ import 'package:bloc_network_exemple/services/user_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
- final UserRepository usersRepository;
+  final UserRepository usersRepository;
 
- UserBloc({this.usersRepository}) : assert(usersRepository != null);
+  UserBloc(this.usersRepository) : super(UserEmptyState());
 
-@override
-UserState get initialState =>  UserEmptyState();
-
-@override
+  @override
   Stream<UserState> mapEventToState(UserEvent event) async* {
-
-  if(event is UserLoadEvent) {
-    yield UserLoadingState();
-    try {
-      final List<User> _loadedUserList = await usersRepository.getAllUsers();
-      yield UserLoadedState(loadedUser: _loadedUserList);
- } catch (_) {
- yield UserErrorState();
- }
- } else if(event is UserCleanEvent) {
- yield UserEmptyState();
- }
- }
+    if (event is UserLoadEvent) {
+      yield UserLoadingState();
+      try {
+        final List<User> _loadedUserList = await usersRepository.getAllUsers();
+        yield UserLoadedState(loadedUser: _loadedUserList);
+      } catch (_) {
+        yield UserErrorState();
+      }
+    } else if (event is UserCleanEvent) {
+      yield UserEmptyState();
+    }
+  }
 }
+
+// class UserBloc extends Bloc<UserEvent, UserState> {
+//  final UserRepository usersRepository;
+//
+//  UserBloc({this.usersRepository}) : assert(usersRepository != null);
+//
+// @override
+// UserState get initialState =>  UserEmptyState();
+//
+// @override
+//   Stream<UserState> mapEventToState(UserEvent event) async* {
+//
+//   if(event is UserLoadEvent) {
+//     yield UserLoadingState();
+//     try {
+//       final List<User> _loadedUserList = await usersRepository.getAllUsers();
+//       yield UserLoadedState(loadedUser: _loadedUserList);
+//  } catch (_) {
+//  yield UserErrorState();
+//  }
+//  } else if(event is UserCleanEvent) {
+//  yield UserEmptyState();
+//  }
+//  }
+// }
